7ddc977ef52023598a836857db753c776a5c6f7f
jasonmiracleg <jasonmiracle15@gmail.com>
2025-01-14 02:42:11 +0700

    Prediction Result Visualization

--- PathFinderWeb/app.py
+++ PathFinderWeb/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 
-with open('../stacked_balanced_weigthed.pkl', 'rb') as file:
+with open('../PathFinder.pkl', 'rb') as file:
     model = pickle.load(file)
 
 app = Flask(__name__)
@@ -24,20 +24,20 @@ def index():
 def predict():
     data = request.form
 
-    soft_skills = [
+    skills = [
         int(data['communication_skills']),
         int(data['teamwork_skills']),
         int(data['presentation_skills']),
         int(data['networking_skills']),
-        int(data['problem_solving_skills'])
+        int(data['problem_solving_skills']),
+        int(data['analytical_skills']),
+        int(data['coding_skills'])
     ]
     
     technical_skills = [
-        int(data['coding_skills']),
         int(data['field_specific_courses']),
         int(data['research_experience']),
         int(data['industry_certifications']),
-        int(data['analytical_skills'])
     ]
     
     proactive_engagement = [
@@ -47,24 +47,24 @@ def predict():
         int(data['internships']),
     ]
     
-    avg_soft_skills = sum(soft_skills) / len(soft_skills)
+    avg_skills = sum(skills) / len(skills)
     avg_technical_skills = sum(technical_skills) / len(technical_skills)
     avg_proactive_engagement = sum(proactive_engagement) / len(proactive_engagement)
     
     # Extract features from the incoming data
     features = [
         float(data['gpa']),
-        avg_soft_skills,
         avg_technical_skills,
-        avg_proactive_engagement
+        avg_proactive_engagement,
+        avg_skills
     ]
     
-    input_data = pd.DataFrame([features], columns=['GPA', 'Soft_Skills', 'Technical_Skills', 'Proactive_Engagement'])
+    input_data = pd.DataFrame([features], columns=['GPA', 'Technical_Skills', 'Proactive_Engagement', 'Skills_Average'])
     prediction = model.predict(input_data)
 
     predicted_field = int(prediction[0])
     # Return the prediction as a JSON response
-    return jsonify({'field': predicted_field})
+    return render_template("result.html", result=predicted_field)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
